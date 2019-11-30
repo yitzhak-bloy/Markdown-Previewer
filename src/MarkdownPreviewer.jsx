b987ea141c395d70ef67188ba2c425fcf6a1821d
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-11-30 20:53:00 +0200

    Finish adding markdown

--- src/MarkdownPreviewer.jsx
+++ src/MarkdownPreviewer.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import marked from 'marked';
 
-marked.setOptions({ //Options for marked.js.
-	sanitize: true, //Sanitizes incoming text.
-	//gfm: true, //Required for breaks.
-	breaks: true, //User using carriage return is interpretted as a new line.
-	//renderer: renderer //Clicking link opens a new tab.
+marked.setOptions({ 
+	breaks: true
 })
 
-// This section is used for clicking a link opens a new tab.
-// const renderer = new marked.Renderer();
-// renderer.link = function (href, title, text) {
-//   return `<a target="_blank" href="${href}">${text}` + '</a>';
-// }
+const renderer = new marked.Renderer();
+renderer.link = function(href, text) {
+  return `<a target="_blank" href=${href}>${text}</a>`;
+};
 
 class MarkdownPreviewer extends Component {
   constructor(props) {
@@ -37,9 +33,7 @@ class MarkdownPreviewer extends Component {
         <h1>Markdown Previewer</h1>
         <textarea id="editor" onChange={this.handleChange} >
         </textarea>
-        <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.text,)}} />
-            {/* {this.state.text} */}
-        {/* </div> */}
+        <div id="preview" dangerouslySetInnerHTML={{__html: marked(this.state.text, { renderer })}} />
       </div>
     )
   }
