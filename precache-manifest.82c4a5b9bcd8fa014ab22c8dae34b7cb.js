c0faa432f824dd7ae83456b4925e5f1971afc4fa
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2019-12-02 14:53:04 +0200

    Updates

---
+++ precache-manifest.82c4a5b9bcd8fa014ab22c8dae34b7cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab27ab6147bb5459d2db99a9713bf685",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "491be317504c69a8529e",
+    "url": "/Markdown-Previewer/static/css/main.baf747dd.chunk.css"
+  },
+  {
+    "revision": "344f5effda4483f960f5",
+    "url": "/Markdown-Previewer/static/js/2.132c27e2.chunk.js"
+  },
+  {
+    "revision": "491be317504c69a8529e",
+    "url": "/Markdown-Previewer/static/js/main.7f669ff4.chunk.js"
+  },
+  {
+    "revision": "dfc88d2931747ded3eae",
+    "url": "/Markdown-Previewer/static/js/runtime-main.8aa3df1a.js"
+  }
+]);
